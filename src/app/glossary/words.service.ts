7476894c9b5d3fcc188b9a4dc86d4fe181efe202
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-10-01 21:08:38 -0400

    Add several words and some styling

--- src/app/glossary/words.service.ts
+++ src/app/glossary/words.service.ts
@@ -25,8 +25,20 @@ export class WordsService implements OnInit {
         definition: "Bible Students: Before 1931, Jehovah’s Witnesses were called Bible Students"
       },
       {
+        word: "Cast lots",
+        definition: "This was a way to decide a question. Lots, or small pieces of wood or stone, were marked and then thrown on the ground or shaken in a container. A decision was made based on how the lots fell or on which lot was pulled out"
+      },
+      {
         word: "Christendom",
         definition: "Christendom: Religions that call themselves Christian but that do not follow the teachings of Christ"
+      },
+      {
+        word: "Conscience",
+        definition: "A sense of right or wrong that we have inside of us. It can stop us from doing wrong things, and it can make us do good things"
+      },
+      {
+        word: "Covenant",
+        definition: "A serious promise or agreement"
       },
       {
         word: "Disfellowship",
@@ -35,6 +47,10 @@ export class WordsService implements OnInit {
       {
         word: "False teachers",
         definition: "False teachers, or apostates: People who rebel against true worship and abandon it"
+      },
+      {
+        word: "Gentile",
+        definition: "A person who is not of the Jewish nation"
       },
       {
         word: "God's day of rest",
@@ -57,8 +73,20 @@ export class WordsService implements OnInit {
         definition: "Perceptive powers (Hebrews 5:14): The ability to think carefully about things and then choose between right and wrong"
       },
       {
+        word: "Ransom",
+        definition: "The price Jehovah paid through Jesus to free humans from sin and death"
+      },
+      {
+        word: "Repent",
+        definition: "To feel regret for past sins and to make the necessary changes to please God"
+      },
+      {
         word: "Sabbath",
         definition: "Sabbath: The seventh day of the week and a day of rest from regular work for the Israelites"
+      },
+      {
+        word: "Sanhedrin",
+        definition: "A Jewish high court"
       },
       {
         word: "Scroll",
@@ -67,6 +95,10 @@ export class WordsService implements OnInit {
       {
         word: "Tribe of Judah",
         definition: "A tribe in Israel. Descendants of Judah, the son of Jacob"
+      },
+      {
+        word: "Undeserved kindness",
+        definition: "Jehovah’s great kindness that he gives freely to sinful humans"
       },
       {
         word: "Unrealities",
